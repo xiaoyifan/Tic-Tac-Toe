e2152363a601e32a639a2a16da87620b93f47398
Yifan Xiao <xiaoyifan@uchicago.edu>
2015-02-06 22:36:02 -0600

    backup

--- TicTacToe/ViewController.m
+++ TicTacToe/ViewController.m
@@ -10,6 +10,9 @@
 
 @interface ViewController ()
 
+@property CGPoint *letterXOriginalPosition;
+@property CGPoint *letterOOriginalPosition;
+
 @end
 
 @implementation ViewController
@@ -17,13 +20,54 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    //self.letterX.translatesAutoresizingMaskIntoConstraints = YES;
+    //self.letterO.translatesAutoresizingMaskIntoConstraints = YES;
+    //using this setting would free the layout constraints.
+    
+    [self disableLetterO];
 }
+
+
+#pragma mark - enable and disable of dragging image
+-(void)disableLetterO{
+    self.letterO.alpha = 0.5;
+    self.letterO.userInteractionEnabled = NO;
+
+}
+// disable lette O, won't accept user interaction
+
+-(void)disableLetterX{
+    self.letterX.alpha = 0.5;
+    self.letterX.userInteractionEnabled = NO;
+    
+}
+// disable lette X, won't accept user interaction
+
+-(void)enableLetterO{
+    self.letterO.alpha = 1;
+    self.letterO.userInteractionEnabled = YES;
+    
+}
+// disable lette O, won't accept user interaction
+
+-(void)enableLetterX{
+    self.letterX.alpha = 1;
+    self.letterX.userInteractionEnabled = YES;
+    
+}
+// disable lette X, won't accept user interaction
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+
+#pragma mark - gesture recognition
 - (IBAction)panLetterX:(UIPanGestureRecognizer *)sender {
     
     CGPoint touchPoint = [sender locationInView:[sender view]];
@@ -74,12 +118,73 @@
 
 -(void)detectIntersect:(UIImageView *)myView{
     
-    for (int i=1; i<9; i++) {
+    for (int i=1; i<=9; i++) {
         UIView *currentView = [[self view] viewWithTag:i];
         if (CGRectIntersectsRect(currentView.frame, myView.frame)) {
             NSLog(@"View Intersect with number %d view", i);
+            
+            
+            //if the intersection is detected, we gonna add the dragging image to the grid
+            if ([self setImageOfView:myView toView:currentView] == FALSE) {
+                
+                //put the animation here and the gradding image will move back to the original position.
+                
+            }
+            else{//if the image insertion is succeeded
+                
+                //detect if there's a winner
+                //if not, we should switch to another player
+                
+                    //tag specification: 20 is X, 10 is O
+                   if (myView.tag == 20) {
+                       [self disableLetterX];
+                       [self enableLetterO];
+                    
+                    }
+                    else if(myView.tag == 10)
+                    {
+                       [self disableLetterO];
+                       [self enableLetterX];
+                    }
+                
+            }
+            
+            break;
+            
         }
     }
+}
+//for reference
+/*
+    work to do:
+    1. animation, if the grid is not empty, the view will roll back to the original position, 
+        Two original position is set as property, get the value, implement the function with roll back
+ 
+    2. animation of growing and shrinking. put the animation effect in a function, and call the function in the 
+        enable function.
+ 
+    3. detect winner
+ 
+    4.
+ 
+ 
+ */
+
+-(Boolean)setImageOfView:(UIImageView *)dragging toView:(UIView *)grid{
+    
+    if (grid.subviews.count == 0) {
+        
+        UIImage *currentImage = dragging.image;
+        UIImageView *subView = [[UIImageView alloc] initWithImage:currentImage];
+        [grid addSubview:subView];
+        
+    }
+    else{
+        return false;
+    }
+    
+    
+    return true;
 }
 
 
